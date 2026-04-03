a50aebfd7e77a917309f7def92fcf882ef240319
SeppuCake <mohdameerulhakeem@gmail.com>
2026-04-03 15:03:46 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-const root = resolve(__dirname, 'src')
-const outDir = resolve(__dirname, 'dist')
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/online-portfolio/',
+  base: "/online-portfolio/",
   plugins: [react()],
-})
+});
