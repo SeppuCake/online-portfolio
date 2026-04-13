10dd0b8fcfa2f3dd08f14a9f48127c38e92cd030
SeppuCake <mohdameerulhakeem@gmail.com>
2026-04-13 16:58:14 +0800

    Update: Added 'Projects' on navbar

--- src/constants/index.js
+++ src/constants/index.js
@@ -61,6 +61,7 @@ import {
 export const navLinks = [
   { id: "about", title: "About" },
   { id: "work", title: "Work" },
+  { id: "projects", title: "Projects" },
   { id: "contact", title: "Contact" },
 ];
 
